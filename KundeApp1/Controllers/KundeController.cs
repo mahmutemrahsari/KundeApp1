c9cff8f810ea76c4d4e6845e6d53cd53e53b11b8
Mahmut Emrah Sari <s306378@oslomet.no>
2020-09-22 17:12:04 +0200

    Add project files.

---
+++ KundeApp1/Controllers/KundeController.cs
@@ -0,0 +1,35 @@
+﻿using KundeApp1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KundeApp1.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class KundeController : ControllerBase
+    {
+        public List<Kunde> HentAlle()
+        {
+            var kundene = new List<Kunde>();
+
+            var kunde1 = new Kunde();
+            kunde1.navn = "Per Hansen";
+            kunde1.adresse = "Osloveien 82";
+
+            var kunde2 = new Kunde
+            {
+                navn = "Line Jensen",
+                adresse = "Askerveien 82"
+            };
+
+
+            kundene.Add(kunde1);
+            kundene.Add(kunde2);
+
+            return kundene;
+        }
+
+    }
+}
---
+++ KundeApp1/Models/Kunde.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KundeApp1.Models
+{
+    public class Kunde
+    {
+        public string navn { get; set; }
+        public string adresse { get; set; }
+    }
+}
